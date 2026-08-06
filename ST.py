29da2ab12bac0ae9a9a7e5f75d158f3aef400066
joestuffz17-coder <joestuffz17@gmail.com>
2026-08-06 19:51:29 -0400

    Update ST.py

--- ST.py
+++ ST.py
@@ -3,7 +3,7 @@ subscriptions = []
 
 while True:
     print()
-    choice = input("1. Add subscription.\n2. View all subscription.\n3. Exit.\n\nChoose: ")
+    choice = input("1. Add subscription.\n2. View all subscription.\n3. Delete subscription.\n4. Exit\nChoose: ")
     print()
     if choice == "1":
         name = input("Enter subscription name: ")
@@ -11,12 +11,18 @@ while True:
         print()
         item = [name, date]
         subscriptions.append(item)
-        print(item[0])
-        print(item[1])
     elif choice == "2":
         for sub in subscriptions:
             print(f"Subscription: {sub[0]}, expires on {sub[1]}.")
     elif choice == "3":
+        for index, sub in enumerate(subscriptions):
+            print()
+            print(f"Subscription: {sub[0]}, expires on {sub[1]}.")
+            print()
+            print(index, sub)
+        delete = input("Which number do you want to delete? ")
+                    
+    elif choice == "4":
         print()
         print("goodbye!")
         break
